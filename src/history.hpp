bd19c5fdb19cdc8657143a755ef1e47280cac8a0
Game <Kitiphol@gmail.com>
2025-04-14 17:20:47 +0700

    finish the history file

--- src/history.hpp
+++ src/history.hpp
@@ -2,6 +2,15 @@
 #define HISTORY_HPP
 
 #include <string>
+#include <vector> 
 
+class History {
+    private:
+        std::vector<std::string> history;
+    public:
+        void add(const std::string& command);
+        std::string getLastCommand() const;
+    };
+    
 
 #endif
